7c22a3b20e2e03780d29cc4b72371f575ed2ba0f
szaroket <mpiestrzynska@interia.pl>
2018-01-16 12:10:25 +0100

    Add prototype to calculate max and min value in dataset

--- JavaScript/k-nearest-neighbor/k-nearest-neighbor.js
+++ JavaScript/k-nearest-neighbor/k-nearest-neighbor.js
@@ -6,10 +6,38 @@ var Node = function(object){
     for(var key in object) {
         this[key] = object[key];
     }
-}
+};
 
 //class for all dataset
 var NodeList = function(k){
     this.nodes = [];
     this.k = k;
-}
+};
+
+//create prototype to find max and min value in dataset
+NodeList.prototype.calculateRanges = function(){
+    this.area = {
+        min: 1000000,
+        max: 0
+    };
+
+    this.room = {
+        min: 1000000,
+        max: 0
+    };
+
+    for(var i in this.nodes){
+        if(this.nodes[i].room < this.room.min){
+            this.room.min = this.nodes[i].room;
+        }
+        if(this.nodes[i].room > this.room.max){
+            this.room.max = this.nodes[i].room;
+        }
+        if(this.nodes[i].area < this.area.min){
+            this.area.min = this.nodes[i].area;
+        }
+        if(this.nodes[i].area > this.area.max){
+            this.area.max = this.nodes[i].area;
+        }
+    }
+};
